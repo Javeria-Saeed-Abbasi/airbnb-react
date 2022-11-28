bd90b16d3e455fbe853b4b66b496789451cb1619
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2022-11-28 18:15:26 +0500

    radio-btn on slider

--- src/component/ExpandNav/Month.js
+++ src/component/ExpandNav/Month.js
@@ -2,12 +2,20 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "./SwiperStyle.css";
-import { ToggleButton, ToggleButtonGroup, ButtonGroup, Image } from "react-bootstrap";
+import {
+  ToggleButton,
+  ToggleButtonGroup,
+  ButtonGroup,
+  Image,
+  Form,
+  FormCheck,
+  InputGroup,
+} from "react-bootstrap";
 import Calendaricon from "../../component/assets/images/calender-icon.jpg";
 
 const Month = () => {
   // const dateToFormat = '2022-11-28T12:59-0500';
-
+  const [check, setcheck] = useState("");
   const [checked, setChecked] = useState(false);
   const [radioValue, setRadioValue] = useState("1");
 
@@ -45,266 +53,165 @@ const Month = () => {
           className="mySwiper"
         >
           <SwiperSlide>
+            <div className="card">
+            <label>
+              <InputGroup type="radio" name="test" id="test" value="business" />
+              <div
+                className="slides"
+                style={{ width: "120px", height: "134px" }}
+              >
+                <div className="slide-content">
+                  <div className="cal-icon text-white">
+                    <Image src={Calendaricon} alt="calendar-icon" />
+                  </div>
+                  <p className="text-dark month">December</p>
+                  <p className="text-dark dates">2022</p>
+                </div>
+              </div>
+            </label>
+            </div>
+
            
-            <div
-              className="slides"
-              style={{ width: "120px", height: "134px" }}
-            >
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="card">
+            <label>
+              <InputGroup type="radio" name="test" id="test" value="business" />
+
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
               <div className="slide-content">
-              <div className="cal-icon text-white">
-              <Image src={Calendaricon} alt="calendar-icon" />
-              
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">January</p>
+                <p className="text-dark dates">2023</p>
               </div>
-              <p className="text-dark month">
-              December
-              </p>
-              <p className="text-dark dates">
-              2022
-              </p>
-              </div>
-          
             </div>
+            </label>
+            </div>
+       
           </SwiperSlide>
           <SwiperSlide>
-          
-            <div
-              className="slides"
-              style={{ width: "120px", height: "134px" }}
-            >
-                 <div className="slide-content">
-              <div className="cal-icon text-white">
-              <Image src={Calendaricon} alt="calendar-icon" />
-              
-              </div>
-              <p className="text-dark month">
-              January
-              </p>
-              <p className="text-dark dates">
-              2023
-              </p>
-              </div>
-            </div>
-          </SwiperSlide>
-           <SwiperSlide>
-          
-            <div
-              className="slides"
-              style={{ width: "120px", height: "134px" }}
-            >
-                 <div className="slide-content">
-              <div className="cal-icon text-white">
-              <Image src={Calendaricon} alt="calendar-icon" />
-              
-              </div>
-              <p className="text-dark month">
-              Feburary
-              </p>
-              <p className="text-dark dates">
-              2023
-              </p>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">Feburary</p>
+                <p className="text-dark dates">2023</p>
               </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">March</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-            <p className="text-dark month">
-            March
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">April</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">May</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-            <p className="text-dark month">
-            April
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">June</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">July</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-            <p className="text-dark month">
-            May
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">August</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">September</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-            <p className="text-dark month">
-            June
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">October</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">November</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-            <p className="text-dark month">
-            July
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="slides" style={{ width: "120px", height: "134px" }}>
+              <div className="slide-content">
+                <div className="cal-icon text-white">
+                  <Image src={Calendaricon} alt="calendar-icon" />
+                </div>
+                <p className="text-dark month">December</p>
+                <p className="text-dark dates">2023</p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
-            </div>
-            <p className="text-dark month">
-            August
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
-            </div>
-            <p className="text-dark month">
-            September
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
-            </div>
-            <p className="text-dark month">
-            October
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
-            </div>
-            <p className="text-dark month">
-            November
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          
-          <div
-            className="slides"
-            style={{ width: "120px", height: "134px" }}
-          >
-               <div className="slide-content">
-            <div className="cal-icon text-white">
-            <Image src={Calendaricon} alt="calendar-icon" />
-            
-            </div>
-            <p className="text-dark month">
-            December
-            </p>
-            <p className="text-dark dates">
-            2023
-            </p>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
